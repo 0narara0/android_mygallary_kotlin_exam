6a0158756545fe40426eb9979f0e81df73fa9a8e
miyoung <0narara0@gmail.com>
2019-05-20 15:25:01 +0900

    ViewPager2 사용해 전자액자 완성

--- app/src/main/java/com/narara/android_mygallary_kotlin_exam/AlertFragment.kt
+++ app/src/main/java/com/narara/android_mygallary_kotlin_exam/AlertFragment.kt
@@ -1,11 +1,15 @@
 package com.narara.android_mygallary_kotlin_exam
 
 
+import android.app.Dialog
+import android.content.DialogInterface
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -13,18 +17,22 @@ import android.view.ViewGroup
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- *
- */
-class AlertFragment : Fragment() {
+                    // 인터페이스 대신에 아래 코드, 생성자 추가한 것이라서 null 체크도 안한다.
+                    // 생성자에 있는 것들은 field 에 있는 것으로 본다
+class AlertFragment(private val onClickListener: () -> Unit) : DialogFragment() {
+//    interface onClickListener {
+//        fun onClick()
+//    }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_alert, container, false)
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("권한이 필요한 이유")
+        builder.setMessage("사진 정보를 얻으려면 외부 저장소 권한이 필수입니다.")
+        builder.setPositiveButton("수락") { _, _ ->
+            onClickListener.invoke()
+        }
+        builder.setNegativeButton("거부", null)
+        return builder.create()
     }
 
 
--- app/src/main/java/com/narara/android_mygallary_kotlin_exam/MainActivity.kt
+++ app/src/main/java/com/narara/android_mygallary_kotlin_exam/MainActivity.kt
@@ -5,12 +5,20 @@ import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.narara.android_mygallary_kotlin_exam.databinding.ItemPhotoBinding
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
     // 글로벌하게 상수 정의 할 때 쓰인다. const val 은 static final 같은 느낌?
     companion object {
+        val TAG: String = MainActivity::class.java.simpleName
         const val REQUEST_READ_EXTERNAL_STORAGE = 1000
     }
 
@@ -18,43 +26,123 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // 권한 체크
+        // 권한 체크, 권한이 부여되었는지 확인
         if (ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.READ_EXTERNAL_STORAGE
-            ) != PackageManager
-                .PERMISSION_GRANTED
+                        this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                ) != PackageManager
+                        .PERMISSION_GRANTED
         ) {
             if (ActivityCompat.shouldShowRequestPermissionRationale(
-                    this,
-                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
-                // 설명
-
+                            this,
+                            Manifest.permission.READ_EXTERNAL_STORAGE
+                    )
+            ) {
+                // 이전에 이미 권한이 거부되었을 때 설명
+                AlertFragment {
+                    // 권한 요청
+                    ActivityCompat.requestPermissions(
+                            this,
+                            // arrayOf는 배열 만드는 것
+                            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                            REQUEST_READ_EXTERNAL_STORAGE
+                    )
+                }.show(supportFragmentManager, "dialog")
             } else {
                 // 권한 요청
                 ActivityCompat.requestPermissions(
-                    this,
-                    // arrayOf는 배열 만드는 것
-                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
-                    REQUEST_READ_EXTERNAL_STORAGE
+                        this,
+                        arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), // arrayOf는 배열 만드는 것
+                        REQUEST_READ_EXTERNAL_STORAGE
                 )
             }
         } else {
             // 권한이 이미 허용 됨
             getAllPhotos()
         }
-
     }
+
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            REQUEST_READ_EXTERNAL_STORAGE -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // 권한 허용됨
+                    getAllPhotos()
+                } else {
+                    // 권한 거부
+                    toast("권한 거부 됨")
+                }
+            }
+        }
+    }
+
 
     // 이미지 정보 가져오기
     private fun getAllPhotos() {
         val cursor = contentResolver
-            .query(
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                null,
-                null,
-                null,
-                "${MediaStore.Images.ImageColumns.DATE_TAKEN} DESC"
-            )
+                .query(
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                        null,
+                        null,
+                        null,
+                        "${MediaStore.Images.ImageColumns.DATE_TAKEN} DESC"
+                )
+
+        val items = arrayListOf<Photo>()
+
+        if (cursor != null) {
+            while (cursor.moveToNext()) {
+                val uri = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA))
+                Log.d(TAG, uri.toString())
+                items.add(Photo(uri))
+            }
+            cursor.close()
+
+        }
+
+        val adapter = PhotoAdapter()
+        adapter.items = items
+        adapter.notifyDataSetChanged()
+
+        view_pager.adapter = adapter
+
+
+        timer(period = 3000) {
+            runOnUiThread {
+                if (view_pager.currentItem < adapter.itemCount -1) {
+                    view_pager.currentItem = view_pager.currentItem +1
+                } else {
+                    view_pager.currentItem = 0
+                }
+            }
+        }
     }
+
+}
+
+// Model
+data class Photo(val uri: String)
+
+// Adapter
+class PhotoAdapter : RecyclerView.Adapter<PhotoAdapter.PhotoViewHolder>() {
+    var items = arrayListOf<Photo>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
+        val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_photo, parent, false)
+        val binding = ItemPhotoBinding.bind(view)
+        return PhotoViewHolder(binding)
+    }
+
+    override fun getItemCount() = items.size
+
+    override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) {
+        holder.binding.photo = items[position]
+    }
+
+    class PhotoViewHolder(val binding: ItemPhotoBinding) : RecyclerView.ViewHolder(binding.root)
+
 }
